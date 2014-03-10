94b9d7e696365035217c7ad75bedfa39c5e36ad6
Jorge Reyero <jorgereyero@gmail.com>
2014-03-10 23:00:59 +0000

    Add methos to user controller

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -3,9 +3,29 @@ var User = require('../models/user.js');
 
 exports.getAll = function (req, res){
 	console.log('getting all users');
-	User.find({}, function (err, users) {
-	  if (err) return console.error(err);
-	  console.log(users);
-	  res.send(users);
-	});
+	return User.find(function (err, products) {
+    if (!err) {
+      return res.send(products);
+    } else {
+      return console.log(err);
+    }
+  });
+};
+
+exports.get = function(req, res){
+	var id = req.params.id;
+	console.log('getting user id: ' + id);
+	return User.findById(id, function (err, user) {
+    if (!err) {
+      return res.send(user);
+    } else {
+      return console.log(err);
+    }
+  });
+};
+
+exports.create = function(req, res){
+	var product;
+	console.log('Post : ');
+	console.log(req.body);
 };
