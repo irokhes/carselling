ae9ef17ab7776747cb3a41880223cde0c214b344
Jorge Reyero <jorgereyero@gmail.com>
2014-03-11 21:48:13 +0000

    Add more action to usercontroller

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -28,5 +28,48 @@ exports.create = function(req, res){
 	var user;
 	console.log('Post : ');
 	console.log(req.body);
-	user = 
+	user = new User({
+		username: req.body.username,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		password: req.body.password,
+		approved: false,
+		banned: false
+	});
+	product.save(function (err) {
+		if (!err) {
+		  return console.log("created");
+		} else {
+		  return console.log(err);
+		}
+	});
+	return res.send(product);	
+};
+
+exports.update = function(req, res){
+	return User.findById(req.params.id, function(err, user){
+		user.firstName = req.body.firstName;
+		user.lastName = req.body.lastName;
+		
+		return user.save(function(err){
+			if(!err){
+				console.log('updated');
+			}else{
+				console.log(err);
+			}
+			res.send(user);
+		});
+	});
+};
+
+exports.delete = function(req, res){
+	return User.findeById(req.params.id, function(err, user){
+		return user.remove(function(err){
+			if(!err){
+				console.log('deleted');
+			}else{
+				console.log(err);
+			}
+		});
+	};
 };
